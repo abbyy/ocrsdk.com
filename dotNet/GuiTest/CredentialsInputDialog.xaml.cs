54d2d866ee804ea971716ee308601d7f7a115e0c
Abbyy <vasily_panferov@abbyy.com>
2011-11-09 16:09:15 +0400

    Message box for user credentials

---
+++ dotNet/GuiTest/CredentialsInputDialog.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GuiTest
+{
+    /// <summary>
+    /// Interaction logic for CredentialsInputDialog.xaml
+    /// </summary>
+    public partial class CredentialsInputDialog : Window
+    {
+        public CredentialsInputDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if( String.IsNullOrEmpty( ApplicationId.Text ) || String.IsNullOrEmpty( Password.Text ) )
+                return;
+
+            this.DialogResult = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+
+    }
+}
