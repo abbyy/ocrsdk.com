98c9fc3e5fe6b032361618ba35cbba6adfcd4e4a
Abbyy <vasily_panferov@abbyy.com>
2011-11-16 17:18:04 +0400

    Java client library and test

---
+++ Java/Abbyy.Ocrsdk.client/src/TestApp.java
@@ -0,0 +1,52 @@
+import com.abbyy.ocrsdk.*;
+
+public class TestApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "App started" );
+		
+		if( args.length != 4 ) {
+			System.out.println( "Invalid arugments. Usage:" );
+			System.out.println( "program <app id> <password> <input file> <output file>" );
+			return;
+		}
+		
+		Client restClient = new Client();
+		restClient.AppId = args[0];
+		restClient.Password = args[1];
+		
+		String filePath = args[2];
+		String outputFile = args[3];
+		
+		ProcessingSettings settings = new ProcessingSettings();
+		
+		try {
+			System.out.println( "Uploading.." );
+			Task task = restClient.ProcessImage(filePath, settings);
+			
+			while( task.IsTaskActive() ) {
+				Thread.sleep(2000);
+				
+				System.out.println( "Waiting.." );
+				task = restClient.GetTaskStatus(task.Id);
+			}
+			
+			if( task.Status == Task.TaskStatus.Completed ) {
+				System.out.println( "Downloading.." );
+				restClient.DownloadResult(task, outputFile);
+			} else {
+				System.out.println( "Task failed" );
+			}
+			
+			System.out.println( "Ready" );
+			
+		} catch( Exception e) {
+			System.out.println( "Exception occured:" + e.getMessage() );
+		}
+	}
+
+}
---
+++ Java/Abbyy.Ocrsdk.client/src/com/abbyy/ocrsdk/ProcessingSettings.java
@@ -0,0 +1,8 @@
+package com.abbyy.ocrsdk;
+
+public class ProcessingSettings {
+	public String AsUrlParams()
+	{
+		return "language=English&exportFormat=pdfSearchable";
+	}
+}
