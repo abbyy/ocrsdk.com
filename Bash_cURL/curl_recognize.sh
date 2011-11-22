cc710e1a7e1c5526b2bde4d2e36a76fb5dbe210c
Abbyy <vasily_panferov@abbyy.com>
2011-11-22 16:05:51 +0400

    Demo sample using bash and curl

---
+++ Bash_cURL/curl_recognize.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Convert an image file by Abbyy Cloud OCR SDK using cURL
+# Usage: cloud_recognize <input> <output> [-l language] [-f txt|rtf|docx|xlsx|pptx|pdfSearchable|pdfTextAndImages|xml]
+
+# do not forget to set http_proxy variable if necessary
+
+ServerUrl='http://cloud.ocrsdk.com'
+Login="_mylogin_"
+Password="_mypassword_";
+
+
+if [ -n $FRE_LOGIN ]; then
+	Login="$FRE_LOGIN";
+fi;
+
+if [ -n $FRE_PWD ]; then
+	Password="$FRE_PWD";
+fi;
+
+
+params=`getopt -o f:l: -- "$@"`
+if [ $? != 0 ] ; then 
+	echo "Invalid arguments. Usage:"
+	echo "$0 <input> <output> [-f output_format] [-l language]"
+	echo "output_format: txt|rtf|docx|xlsx|pptx|pdfSearchable|pdfTextAndImages|xml"
+       	exit 1;
+fi
+
+OutFormat="txt"
+Language="english"
+
+eval set -- "$params"
+while true; do
+	case "$1" in
+		-f) OutFormat="$2"; shift 2;;
+		-l) Language="$2"; shift 2;;
+		--) shift;;
+		*) if [ -z $1 ]; then
+			break;
+		   elif [ -z $SourceFile ]; then
+			SourceFile=$1;
+		   elif [ -z $TargetFile ]; then
+			TargetFile=$1;
+		   else 
+			echo "Invalid argument: $1" >&2;
+			exit 1;
+		   fi
+		   shift;;
+	esac
+		
+done
+
+if [ -z $TargetFile ]; then
+	echo "Invalid arguments" >&2;
+	exit 1;
+fi
+
+echo "Uploading.."
+response=`curl -s -S --user $Login:$Password --form upload=@$SourceFile $ServerUrl/processImage?exportFormat=$OutFormat&language=$Language`
+
+
+#Select guid from response string
+taskId=`echo $response | grep -o -E 'task id="[^"]*"' | cut -d '"' -f 2`
+if [ -z $taskId ]; then
+	echo "Error uploading file" >&2;
+	exit 1;
+fi
+
+echo "Uploaded, task id is '$taskId'"
+
+# Wait until image is processed
+taskStatus="Queued"
+echo -n "Waiting.."
+while [ $taskStatus != "Completed" ]
+do
+	sleep 4
+	echo -n "."
+	response=`curl -s -S --user $Login:$Password $ServerUrl/getTaskStatus?taskId=$taskId`
+	taskStatus=`echo $response | grep -o -E 'status="[^"]+"' | cut -d '"' -f 2`
+done 
+
+echo
+
+# Get result url
+resultUrl=`echo $response | grep -o -E 'resultUrl="[^"]+"' | cut -d '"' -f 2`
+
+# Get result
+response=`curl -s -S -o $TargetFile $resultUrl`
+echo "Done."
