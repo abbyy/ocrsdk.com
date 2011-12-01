e9192b99c68a060c65abefe430762703848dff8c
Whalebot Helmsman <whalebot.helmsman@gmail.com>
2011-12-01 23:07:19 +0400

    fix formatting

--- Bash_cURL/curl_recognize.sh
+++ Bash_cURL/curl_recognize.sh
@@ -11,20 +11,20 @@ Password="_mypassword_";
 
 
 if [ -n $ABBYY_APPID ]; then
-	ApplicationId="$ABBYY_APPID";
+    ApplicationId="$ABBYY_APPID";
 fi;
 
 if [ -n $ABBYY_PWD ]; then
-	Password="$ABBYY_PWD";
+    Password="$ABBYY_PWD";
 fi;
 
 
 params=`getopt -o f:l: -- "$@"`
-if [ $? != 0 ] ; then 
-	echo "Invalid arguments. Usage:"
-	echo "$0 <input> <output> [-f output_format] [-l language]"
-	echo "output_format: txt|rtf|docx|xlsx|pptx|pdfSearchable|pdfTextAndImages|xml"
-       	exit 1;
+if [ $? != 0 ] ; then
+    echo "Invalid arguments. Usage:"
+    echo "$0 <input> <output> [-f output_format] [-l language]"
+    echo "output_format: txt|rtf|docx|xlsx|pptx|pdfSearchable|pdfTextAndImages|xml"
+    exit 1;
 fi
 
 OutFormat="txt"
@@ -32,28 +32,27 @@ Language="english"
 
 eval set -- "$params"
 while true; do
-	case "$1" in
-		-f) OutFormat="$2"; shift 2;;
-		-l) Language="$2"; shift 2;;
-		--) shift;;
-		*) if [ -z $1 ]; then
-			break;
-		   elif [ -z $SourceFile ]; then
-			SourceFile=$1;
-		   elif [ -z $TargetFile ]; then
-			TargetFile=$1;
-		   else 
-			echo "Invalid argument: $1" >&2;
-			exit 1;
-		   fi
-		   shift;;
-	esac
-		
+    case "$1" in
+        -f) OutFormat="$2"; shift 2;;
+        -l) Language="$2"; shift 2;;
+        --) shift;;
+        *) if [ -z $1 ]; then
+            break;
+        elif [ -z $SourceFile ]; then
+            SourceFile=$1;
+        elif [ -z $TargetFile ]; then
+            TargetFile=$1;
+        else
+            echo "Invalid argument: $1" >&2;
+            exit 1;
+        fi
+        shift;;
+    esac
 done
 
 if [ -z $TargetFile ]; then
-	echo "Invalid arguments" >&2;
-	exit 1;
+    echo "Invalid arguments" >&2;
+    exit 1;
 fi
 
 echo "Uploading.."
@@ -63,8 +62,8 @@ response=`curl -s -S --user $ApplicationId:$Password --form upload=@$SourceFile 
 #Select guid from response string
 taskId=`echo $response | grep -o -E 'task id="[^"]*"' | cut -d '"' -f 2`
 if [ -z $taskId ]; then
-	echo "Error uploading file" >&2;
-	exit 1;
+    echo "Error uploading file" >&2;
+    exit 1;
 fi
 
 echo "Uploaded, task id is '$taskId'"
@@ -74,11 +73,11 @@ taskStatus="Queued"
 echo -n "Waiting.."
 while [ $taskStatus != "Completed" ]
 do
-	sleep 4
-	echo -n "."
-	response=`curl -s -S --user $ApplicationId:$Password $ServerUrl/getTaskStatus?taskId=$taskId`
-	taskStatus=`echo $response | grep -o -E 'status="[^"]+"' | cut -d '"' -f 2`
-done 
+    sleep 4
+    echo -n "."
+    response=`curl -s -S --user $ApplicationId:$Password $ServerUrl/getTaskStatus?taskId=$taskId`
+    taskStatus=`echo $response | grep -o -E 'status="[^"]+"' | cut -d '"' -f 2`
+done
 
 echo
 
