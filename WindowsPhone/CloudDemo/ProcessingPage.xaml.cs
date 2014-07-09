a788740f1e9eef2801f1a86cece842a35eaef0ac
Dmitry-Me <wipedout@yandex.ru>
2014-07-09 15:10:19 +0400

    Link to FAQ section on where to find the application id and password.

--- WindowsPhone/CloudDemo/ProcessingPage.xaml.cs
+++ WindowsPhone/CloudDemo/ProcessingPage.xaml.cs
@@ -28,6 +28,11 @@ namespace CloudDemo
 
             RestServiceClient syncClient = new RestServiceClient();
             #error Please provide application id and password and remove this line !!!
+            // To create an application and obtain a password,
+            // register at http://cloud.ocrsdk.com/Account/Register
+            // More info on getting your application id and password at
+            // http://ocrsdk.com/documentation/faq/#faq3
+
 			// Name of application you created
             syncClient.ApplicationId = "";
 			// Password should be sent to your e-mail after application was created
