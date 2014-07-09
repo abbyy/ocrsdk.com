01091f6d8ad57130e5ca3fd685e914c227ac4b0b
Dmitry-Me <wipedout@yandex.ru>
2014-07-09 18:18:45 +0400

    Comments on waiting for tasks, optimized waiting code.

--- Ruby/abbyy_ruby_example.rb
+++ Ruby/abbyy_ruby_example.rb
@@ -62,32 +62,46 @@ rescue RestClient::ExceptionWithResponse => e
 else
   # Get task id from response xml to check task status later
   xml_data = REXML::Document.new(response)
-  task_id = xml_data.elements["response/task"].attributes["id"]
+  task_element = xml_data.elements["response/task"]
+  task_id = task_element.attributes["id"]
+  # Obtain the task status here so that the loop below is not started
+  # if your application account has not enough credits
+  task_status = task_element.attributes["status"]
 end
 
 # Get task information in a loop until task processing finishes
-puts "Processing image.."
-begin
-  # Make a small delay
-  sleep(0.5)
+puts "Waiting till image is processed.."
+while task_status == "InProgress" or task_status == "Queued" do
+  begin
+    # Note: it's recommended that your application waits
+    # at least 2 seconds before making the first getTaskStatus request
+    # and also between such requests for the same task.
+    # Making requests more often will not improve your application performance.
+    # Note: if your application queues several files and waits for them
+    # it's recommended that you use listFinishedTasks instead (which is described
+    # at http://ocrsdk.com/documentation/apireference/listFinishedTasks/).
+    # Wait a bit
+    sleep(5)
   
-  # Call the getTaskStatus function (see http://ocrsdk.com/documentation/apireference/getTaskStatus)
-  response = RestClient.get("#{BASE_URL}/getTaskStatus?taskid=#{task_id}")
-rescue RestClient::ExceptionWithResponse => e
-  # Show getTaskStatus errors
-  output_response_error(e.response)
-  raise
-else
-  # Get the task status from response xml
-  xml_data = REXML::Document.new(response)
-  task_status = xml_data.elements["response/task"].attributes["status"]
+    # Call the getTaskStatus function (see http://ocrsdk.com/documentation/apireference/getTaskStatus)
+    response = RestClient.get("#{BASE_URL}/getTaskStatus?taskid=#{task_id}")
+  rescue RestClient::ExceptionWithResponse => e
+    # Show getTaskStatus errors
+    output_response_error(e.response)
+    raise
+  else
+    # Get the task status from response xml
+    xml_data = REXML::Document.new(response)
+    task_element = xml_data.elements["response/task"]
+    task_status = task_element.attributes["status"]
+  end
+end
+
+# Check if there were errors ..
+raise "The task hasn't been processed because an error occurred" if task_status == "ProcessingFailed"
   
-  # Check if there were errors ..
-  raise "The task hasn't been processed because an error occurred" if task_status == "ProcessingFailed"
-  
-  # .. or you don't have enough credits (see http://ocrsdk.com/documentation/specifications/task-statuses for other statuses)
-  raise "You don't have enough money on your account to process the task" if task_status == "NotEnoughCredits"
-end until task_status == "Completed"
+# .. or you don't have enough credits (see http://ocrsdk.com/documentation/specifications/task-statuses for other statuses)
+raise "You don't have enough money on your account to process the task" if task_status == "NotEnoughCredits"
 
 # Get the result download link
 download_url = xml_data.elements["response/task"].attributes["resultUrl"]
