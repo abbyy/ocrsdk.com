c00f63c5493aac32f64537a8981e16af308487c8
Peter Rekdal Sunde <peter.sunde@gmail.com>
2015-08-19 15:18:02 +0200

    Add overloads to ProcessImage.

--- dotNet/dotNetWrapper/RestServiceClient.cs
+++ dotNet/dotNetWrapper/RestServiceClient.cs
@@ -217,7 +217,36 @@ namespace Abbyy.CloudOcrSdk
         /// <exception cref="ProcessingErrorException">thrown when something goes wrong</exception>
         public Task ProcessImage(string filePath, ProcessingSettings settings)
         {
-            string url = String.Format("{0}/processImage?{1}", ServerUrl,  settings.AsUrlParams);
+            using (var ms = new MemoryStream())
+            {
+                var bytes = File.ReadAllBytes(filePath);
+                ms.Write(bytes, 0, bytes.Length);
+                return ProcessImage(ms, settings);
+            }
+        }
+
+        /// <summary>
+        /// Upload a file to service synchronously and start processing
+        /// </summary>
+        /// <param name="inputStream">Stream to process</param>
+        /// <param name="settings">Language and output format</param>
+        /// <returns>Id of the task. Check task status to see if you have enough units to process the task</returns>
+        /// <exception cref="ProcessingErrorException">thrown when something goes wrong</exception>
+        public Task ProcessImage(Stream inputStream, ProcessingSettings settings)
+        {
+            return ProcessImage(toByteArray(inputStream), settings);
+        }
+
+        /// <summary>
+        /// Upload a file to service synchronously and start processing
+        /// </summary>
+        /// <param name="input">Byte array to process</param>
+        /// <param name="settings">Language and output format</param>
+        /// <returns>Id of the task. Check task status to see if you have enough units to process the task</returns>
+        /// <exception cref="ProcessingErrorException">thrown when something goes wrong</exception>
+        public Task ProcessImage(byte[] input, ProcessingSettings settings)
+        {
+            string url = String.Format("{0}/processImage?{1}", ServerUrl, settings.AsUrlParams);
 
             if (!String.IsNullOrEmpty(settings.Description))
             {
@@ -228,21 +257,21 @@ namespace Abbyy.CloudOcrSdk
             {
                 // Build post request
                 WebRequest request = createPostRequest(url);
-                writeFileToRequest(filePath, request);
+                writeFileToRequest(input, request);
 
                 XDocument response = performRequest(request);
                 Task task = ServerXml.GetTaskStatus(response);
 
                 return task;
             }
-            catch (System.Net.WebException e )
+            catch (System.Net.WebException e)
             {
-                String friendlyMessage = retrieveFriendlyMessage( e );
-				if (friendlyMessage != null)
-				{
-					throw new ProcessingErrorException(friendlyMessage, e);
-				}
-				throw new ProcessingErrorException("Cannot upload file", e);
+                String friendlyMessage = retrieveFriendlyMessage(e);
+                if (friendlyMessage != null)
+                {
+                    throw new ProcessingErrorException(friendlyMessage, e);
+                }
+                throw new ProcessingErrorException("Cannot upload file", e);
             }
         }
 
@@ -572,6 +601,64 @@ namespace Abbyy.CloudOcrSdk
 
         #region Request management functions
 
+        internal static byte[] toByteArray(Stream stream)
+        {
+            var memoryStream = stream as MemoryStream;
+            if (memoryStream != null)
+            {
+                return memoryStream.ToArray();
+            }
+
+            long originalPosition = 0;
+
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[4096];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
+        }
+
         private static void copyStream(Stream input, Stream output)
         {
             byte[] buffer = new byte[8 * 1024];
@@ -633,20 +720,29 @@ namespace Abbyy.CloudOcrSdk
         }
 
         private void writeFileToRequest( string filePath, WebRequest request )
-		{
-			using( BinaryReader reader = new BinaryReader( File.OpenRead( filePath ) ) ) {
-				request.ContentLength = reader.BaseStream.Length;
-				using( Stream stream = request.GetRequestStream() ) {
-					byte[] buf = new byte[reader.BaseStream.Length];
-					while( true ) {
-						int bytesRead = reader.Read( buf, 0, buf.Length );
-						if( bytesRead == 0 ) {
-							break;
-						}
-						stream.Write( buf, 0, bytesRead );
-					}
-				}
-			}
+        {
+            writeFileToRequest(File.ReadAllBytes(filePath), request);
+        }
+
+        private void writeFileToRequest(byte[] input, WebRequest request)
+        {
+            using (MemoryStream ms = new MemoryStream(input))
+            {
+                request.ContentLength = ms.Length;
+                using (Stream stream = request.GetRequestStream())
+                {
+                    byte[] buf = new byte[ms.Length];
+                    while (true)
+                    {
+                        int bytesRead = ms.Read(buf, 0, buf.Length);
+                        if (bytesRead == 0)
+                        {
+                            break;
+                        }
+                        stream.Write(buf, 0, bytesRead);
+                    }
+                }
+            }
         }
 
         #endregion
